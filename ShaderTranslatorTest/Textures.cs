e687a3bc3724967a8166ed13528a8ba3c3b4999f
Georg Wicke <georg.wicke@rwth-aachen.de>
2019-08-28 09:30:02 +0200

    Ignore unimplemented test

--- ShaderTranslatorTest/Textures.cs
+++ ShaderTranslatorTest/Textures.cs
@@ -10,7 +10,7 @@ namespace ShaderTranslator.Test
     class Textures : Base
     {
         Vector4 Shader(Vector2 texCoord, [ShaderResource(0, 0)] Texture2D texture) => texture.Sample(texCoord);
-        [Test]
+        [Test, Ignore("Not implemented yet, not sure whether it will ever be")]
         public void UsingParameters() => TestPixelShader<Vector2, Texture2D, Vector4>(Shader);
 
         [ShaderResource(0, 0)]
