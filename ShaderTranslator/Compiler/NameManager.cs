6fe868b4cd150487dfb7659e02d1577f6213834a
Georg Wicke <georg.wicke@rwth-aachen.de>
2019-09-18 15:06:09 +0200

    Changed keywords to GLSL

--- ShaderTranslator/Compiler/NameManager.cs
+++ ShaderTranslator/Compiler/NameManager.cs
@@ -9,36 +9,35 @@ namespace ShaderTranslator
     {
         HashSet<string> keywords = new HashSet<string>
         {
-            //Source: https://docs.microsoft.com/en-us/windows/win32/direct3dhlsl/dx-graphics-hlsl-appendix-keywords
-            "AppendStructuredBuffer", "asm", "asm_fragment",
-            "BlendState", "bool", "break", "Buffer", "ByteAddressBuffer",
-            "case", "cbuffer", "centroid", "class", "column_major", "compile", "compile_fragment", "CompileShader", "const", "continue", "ComputeShader", "ConsumeStructuredBuffer",
-            "default", "DepthStencilState", "DepthStencilView", "discard", "do", "double", "DomainShader", "dword",
-            "else", "export", "extern",
-            "false", "float", "for", "fxgroup",
-            "GeometryShader", "groupshared",
-            "half", "Hullshader",
-            "if", "in", "inline", "inout", "InputPatch", "int", "interface",
-            "line", "lineadj", "linear", "LineStream",
-            "matrix", "min16float", "min10float", "min16int", "min12int", "min16uint",
-            "namespace", "nointerpolation", "noperspective", "NULL",
-            "out", "OutputPatch",
-            "packoffset", "pass", "pixelfragment", "PixelShader", "point", "PointStream", "precise",
-            "RasterizerState", "RenderTargetView", "return", "register", "row_major", "RWBuffer", "RWByteAddressBuffer", "RWStructuredBuffer", "RWTexture1D", "RWTexture1DArray", "RWTexture2D", "RWTexture2DArray", "RWTexture3D",
-            "sample", "sampler", "SamplerState", "SamplerComparisonState", "shared", "snorm", "stateblock", "stateblock_state", "static", "string", "struct", "switch", "StructuredBuffer",
-            "tbuffer", "technique", "technique10", "technique11", "texture", "Texture1D", "Texture1DArray", "Texture2D", "Texture2DArray", "Texture2DMS", "Texture2DMSArray", "Texture3D", "TextureCube", "TextureCubeArray", "true", "typedef", "triangle", "triangleadj", "TriangleStream",
-            "uint", "uniform", "unorm", "unsigned",
-            "vector", "vertexfragment", "VertexShader", "void", "volatile",
-            "while",
-
-            "vec2", "vec3", "vec4", "input", "output"
+            //Source: https://stackoverflow.com/questions/6232153/what-keywords-glsl-introduce-to-c
+            "attribute", "uniform", "varying", "layout", "centroid", "flat", "smooth", "noperspective",
+            "patch", "sample", "subroutine", "in", "out", "inout", "invariant", "discard", "mat2", "mat3",
+            "mat4", "dmat2", "dmat3", "dmat4", "mat2x2", "mat2x3", "mat2x4", "dmat2x2", "dmat2x3", "dmat2x4",
+            "mat3x2", "mat3x3", "mat3x4", "dmat3x2", "dmat3x3", "dmat3x4", "mat4x2", "mat4x3", "mat4x4",
+            "dmat4x2", "dmat4x3", "dmat4x4", "vec2", "vec3", "vec4", "ivec2", "ivec3", "ivec4", "bvec2",
+            "bvec3", "bvec4", "dvec2", "dvec3", "dvec4", "uvec2", "uvec3", "uvec4", "lowp", "mediump",
+            "highp", "precision", "sampler1D", "sampler2D", "sampler3D", "samplerCube", "sampler1DShadow",
+            "sampler2DShadow", "samplerCubeShadow", "sampler1DArray", "sampler2DArray", "sampler1DArrayShadow",
+            "sampler2DArrayShadow", "isampler1D", "isampler2D", "isampler3D", "isamplerCube", "isampler1DArray",
+            "isampler2DArray", "usampler1D", "usampler2D", "usampler3D", "usamplerCube", "usampler1DArray",
+            "usampler2DArray", "sampler2DRect", "sampler2DRectShadow", "isampler2DRect", "usampler2DRect",
+            "samplerBuffer", "isamplerBuffer", "usamplerBuffer", "sampler2DMS", "isampler2DMS", "usampler2DMS",
+            "sampler2DMSArray", "isampler2DMSArray", "usampler2DMSArray", "samplerCubeArray", "samplerCubeArrayShadow",
+            "isamplerCubeArray", "usamplerCubeArray", "common", "partition", "active", "asm", "class", "union", "enum",
+            "typedef", "template", "this", "packed", "goto", "inline", "noinline", "volatile", "public", "static",
+            "extern", "external", "interface", "long", "short", "half", "fixed", "unsigned", "superp", "input",
+            "output", "hvec2", "hvec3", "hvec4", "fvec2", "fvec3", "fvec4", "sampler3DRect", "filter", "image1D",
+            "image2D", "image3D", "imageCube", "iimage1D", "iimage2D", "iimage3D", "iimageCube", "uimage1D", "uimage2D",
+            "uimage3D", "uimageCube", "image1DArray", "image2DArray", "iimage1DArray", "iimage2DArray", "uimage1DArray",
+            "uimage2DArray", "image1DShadow", "image2DShadow", "image1DArrayShadow", "image2DArrayShadow", "imageBuffer",
+            "iimageBuffer", "uimageBuffer", "sizeof", "cast", "namespace", "using", "row_major"
         };
 
         public bool IsKeyword(string name) => keywords.Contains(name);
 
         Regex[] toBeRemoved = new Regex[] {
-            new Regex("^_+"), // leading underscored
-            new Regex("__+"), // duplicate underscores
+            new Regex("^_+"), // leading underscores
+            new Regex("(?<=_)_+"), // duplicate underscores
             new Regex("[^a-zA-Z0-9]+"), // non-alphanumeric characters
             new Regex("^\\d"), // leading digits
         };
